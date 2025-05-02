a13d30370156e1cb2feb80fc17e8ff7f09e176f1
nnukalakumar <nnukalakumar@github.com>
2025-05-02 07:08:24 +0000

    final shell

--- kill_container.sh
+++ kill_container.sh
@@ -1,2 +1,12 @@
+s_container_id=$(docker ps -a |grep mysite |grep -i up |awk '{print $1}')
+echo $s_container_id
+if [ -n "$s_container_id"];then
+
+	ehco "container is running $s_container_id"
+	exit 0
+
+else
 container_id=$(docker ps -a |grep mysite |grep -i exited |awk '{print $1}')
 docker rm "$container_id"
+
+fi
