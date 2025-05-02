54ccb43323153647da8588d40d455adaa7772ac7
nnukalakumar <nnukalakumar@github.com>
2025-05-02 07:17:39 +0000

    final shellsc

--- kill_container.sh
+++ kill_container.sh
@@ -2,11 +2,14 @@ s_container_id=$(docker ps -a |grep mysite |grep -i up |awk '{print $1}')
 echo $s_container_id
 if [ -z "$s_container_id" ];then
 
-	ehco "container is running $s_container_id"
+	echo "container is running $s_container_id"
+	echo "test"
 	exit 0
 
 else
+	echo "kill test"
 container_id=$(docker ps -a |grep mysite |grep -i exited |awk '{print $1}')
 docker rm "$container_id"
+	echo "final test"
 
 fi
